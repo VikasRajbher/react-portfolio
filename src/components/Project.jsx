1d36873e8c2114a4556ca6d15b65f5edb3d7bc5a
VikasRajbher <vikasrajbher@gmail.com>
2026-02-17 17:38:28 +0530

    work updated

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,34 @@
-import postImage from "../assets/images/post.png";
+import moody from "../assets/images/moody.png";
 import "remixicon/fonts/remixicon.css";
 
 const Project = () => {
+  const projects = [
+    {
+      id: 1,
+      title: "Moody Player",
+      image: moody,
+      link: "https://moodyplayer-1-7cw1.onrender.com/",
+    },
+  ];
+
   return (
-    <div
-      className="aspect-square bg-cover bg-center rounded-lg relative shadow-lg border-white border-4 cursor-pointer bg-amber-600 group"
-      style={{ backgroundImage: `url(${postImage})` }}
-    >
-      <a href="#">
-        <div className="bg-white w-11/12 rounded-md  absolute bottom-5 left-1/2 -translate-x-1/2 py-1 px-3 flex items-center justify-between duration-500 group-hover:bottom-7 hover:text-green-500 ">
-          <h2 className="text-xs sm:text-sm font-medium">Frontend Project</h2>
-          <i className="ri-send-ins-fill px-1 text-center border-2 rounded-full"></i>
+    <div className="min-h-2/3 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] sm:grid-cols-3 md:grid-cols-4 my-10 gap-5">
+      {projects.map((project) => (
+        <div
+          key={project.id}
+          className="aspect-square bg-cover bg-center rounded-lg relative shadow-lg border-gray-500 dark:border-white border-4 cursor-pointer group"
+          style={{ backgroundImage: `url(${project.image})` }}
+        >
+          <a href={project.link} target="_blank" rel="noopener noreferrer">
+            <div className="bg-white w-11/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-1 px-3 flex items-center justify-between duration-500 group-hover:bottom-7 border border-gray-300">
+              <h2 className="text-xs sm:text-sm font-medium text-black">
+                {project.title}
+              </h2>
+              <i className="ri-send-ins-fill px-1 text-center border-2 rounded-full text-black"></i>
+            </div>
+          </a>
         </div>
-      </a>
+      ))}
     </div>
   );
 };
--- src/sections/MyWork.jsx
+++ src/sections/MyWork.jsx
@@ -5,12 +5,7 @@ const MyWork = () => {
   return (
     <div id="work" className="w-full px-[10%] py-10 min-h-1/2 scroll-mt-20">
       <h2 className="text-center text-4xl font-semibold">My Work</h2>
-      <div className="min-h-2/3 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] sm:grid-cols-3 md:grid-cols-4 my-10 gap-5">
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-      </div>
+      <Project />
       <div className="text-center  p-2">
         <a href="" className="">
           CLick here to see more my projects
